2e7e7aa7b95ebb511fb8e7a107284178f194b958
kainy ryu <kainy2109@gmail.com>
2022-03-31 00:38:51 +0900

    separate keyframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import Styled from 'styled-components';
+import Styled, { keyframes } from 'styled-components';
 
 const Container = Styled.div`
   text-align: center;
@@ -17,28 +17,28 @@ font-size: calc(10px + 2vmin);
 color: white;
 `;
 
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
 const AppLogo = Styled.img`
   height: 40vmin;
   pointer-events: none;
 
   @media (prefers-reduced-motion: no-preference) {
     .App-logo {
-      animation: App-logo-spin infinite 20s linear;
-    }
-  }
-
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
+      animation: ${spin} infinite 20s linear;
     }
   }
 `;
 
 const AppLink = Styled.a`
-color: #61dafb;
+  color: #61dafb;
 `;
 
 function App() {
