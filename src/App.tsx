df7883af6fe29d812e60eebe82ce076ccf8f90be
kainy ryu <kainy2109@gmail.com>
2022-05-02 07:17:30 +0900

    create list page

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,16 @@ import React from 'react';
 import Styled from 'styled-components';
 
 import { ToDoListProvider } from 'Contexts';
-import InputContainer from 'Components/InputContainer';
-import ToDoList from 'Components/ToDoList';
+import List from 'Pages/List';
 import { Route, Routes } from 'react-router-dom';
 
 const Container = Styled.div`
   min-height: 100vh;
+  background-color: #EEEEEE;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-`;
-
-const Contents = Styled.div`
-  display: flex;
-  background-color: #ffffff;
-  flex-direction: column;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
 `;
 
 function App() {
@@ -29,10 +20,7 @@ function App() {
       <Container>
         <Routes>
           <Route path="/">
-            <Contents>
-              <ToDoList />
-              <InputContainer />
-            </Contents>
+            <List />
           </Route>
         </Routes>
       </Container>
---
+++ src/Pages/List/index.tsx
@@ -0,0 +1,47 @@
+import ToDoList from 'Components/ToDoList';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Styled from 'styled-components';
+
+const Container = Styled.div`
+  display: flex;
+  background-color: #ffffff;
+  flex-direction: column;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+  position: relative;
+  align-items: center;
+`;
+
+const AddButton = Styled(Link)`
+  font-size: 20px;
+  color: #FFFFFF;
+  display: flex;
+  align-item: center;
+  justify-content: center;
+  justify-content: center;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  cursor: pointer;
+  position: absolute;
+  bottom: -30px;
+  background-color: #304FFE;
+  box-shadow:5px 5px 10px rgba(0,0,0,0.2);
+  text-decoration: onne;
+  &:hover {
+    background-color: #1E40FE;
+  }
+  &:active {
+    box-shadow: inset 5px 5px 10px rgba(0,0,0,0.2);
+  }
+`;
+export default function index() {
+  return (
+    <Container>
+      <ToDoList />
+      <AddButton to="/add">+</AddButton>
+    </Container>
+  );
+}
