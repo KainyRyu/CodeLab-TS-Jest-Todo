867831dab991ca69a3fb99b1ce5513473a27c757
kainy ryu <kainy2109@gmail.com>
2022-05-01 22:45:13 +0900

    input container test

--- src/Components/index.test.tsx
+++ src/Components/index.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import 'jest-styled-components';
 
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import InputContainer from './InputContainer';
+import { ToDoListProvider } from 'src/Contexts';
 
 describe('<InputContainer/>', () => {
   it('renders component correctly', () => {
@@ -14,5 +15,34 @@ describe('<InputContainer/>', () => {
     expect(button).toBeInTheDocument();
 
     expect(container).toMatchSnapshot();
+  });
+
+  it('empties data after adding data', () => {
+    render(<InputContainer />);
+
+    const input = screen.getByPlaceholderText('Add your todos') as HTMLInputElement;
+    const button = screen.getByText('Add');
+
+    expect(input.value).toBe('');
+    fireEvent.change(input, { target: { value: 'study react 1' } });
+    expect(input.value).toBe('study react 1');
+    fireEvent.click(button);
+    expect(input.value).toBe('');
+  });
+
+  it('adds input data to localStorage via Context', () => {
+    render(
+      <ToDoListProvider>
+        <InputContainer />
+      </ToDoListProvider>,
+    );
+
+    const input = screen.getByPlaceholderText('Add your todos') as HTMLInputElement;
+    const button = screen.getByText('Add');
+
+    fireEvent.change(input, { target: { value: 'study react 1' } });
+    fireEvent.click(button);
+
+    expect(localStorage.getItem('ToDoList')).toBe('["study react 1"]');
   });
 });
