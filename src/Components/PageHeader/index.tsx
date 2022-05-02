263c92772b3b872c128b0b166f3b1833272699ab
kainy ryu <kainy2109@gmail.com>
2022-05-02 19:13:05 +0900

    create header

---
+++ src/Components/PageHeader/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link, useLocation, matchPath } from 'react-router-dom';
+import Styled from 'styled-components';
+
+const Container = Styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: dodgerblue;
+`;
+
+const Title = Styled.div`
+padding: 20px;
+color: #ffffff;
+font-size: 20px;
+font-weight: 600;
+
+`;
+const GoBack = Styled(Link)`
+  padding: 20px;
+  color: #ffffff;
+  font-size: 16px
+  font-weight: 600;
+  text-decoration: none;
+  position: absolute;
+  left: 20px;
+`;
+
+export default function PageHeader() {
+  const { pathname } = useLocation();
+  let title = 'error';
+
+  if (pathname === '/') {
+    title = 'to do list';
+  } else if (pathname === '/add') {
+    title = 'add to do';
+  } else if (pathname.startsWith('/detail')) {
+    title = 'detail';
+  }
+  return (
+    <Container>
+      <Title>{title}</Title>
+      {pathname !== '/' && <GoBack to="/">Go Back</GoBack>}
+    </Container>
+  );
+}
