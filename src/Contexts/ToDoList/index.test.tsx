463a18f6e9f1f396e60eb32f99b55123d21349fd
kainy ryu <kainy2109@gmail.com>
2022-05-04 11:52:53 +0900

    test - ToDoList with router

--- src/Contexts/ToDoList/index.test.tsx
+++ src/Contexts/ToDoList/index.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { ToDoListProvider, useToDoList } from '.';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { ToDoItem } from 'Components';
+import ToDoList from 'Components/ToDoList';
 
 beforeEach(() => {
   localStorage.clear();
@@ -10,15 +13,19 @@ describe('ToDoList Context', () => {
   it('renders component correctly', () => {
     const ChildComponent = () => <div>Child Component</div>;
 
-    render(
-      <ToDoListProvider>
-        <ChildComponent />
-      </ToDoListProvider>,
+    const { container } = render(
+      <Router>
+        <ToDoListProvider>
+          <ChildComponent />
+        </ToDoListProvider>
+      </Router>,
     );
 
     const childComponent = screen.getByText('Child Component');
     expect(childComponent).toBeInTheDocument();
     expect(localStorage.getItem('ToDoList')).toBeNull();
+
+    expect(container).toMatchSnapshot();
   });
 
   it('loads localStorage data and sets it to State', () => {
@@ -28,17 +35,26 @@ describe('ToDoList Context', () => {
       const { toDoList } = useToDoList();
       return (
         <div>
-          {toDoList.map((toDo) => (
-            <div key={toDo}>{toDo}</div>
+          {toDoList.map((toDo, index) => (
+            <ToDoItem key={toDo} id={index} label={toDo} />
           ))}
         </div>
       );
     };
     render(
-      <ToDoListProvider>
-        <ChildComponent />
-      </ToDoListProvider>,
+      <Router>
+        <ToDoListProvider>
+          <ChildComponent />
+        </ToDoListProvider>
+      </Router>,
     );
+
+    const toDoItem1 = screen.getByText('ToDo 1');
+    expect(toDoItem1.getAttribute('href')).toBe('/detail/0');
+    const toDoItem2 = screen.getByText('ToDo 2');
+    expect(toDoItem2.getAttribute('href')).toBe('/detail/1');
+    const toDoItem3 = screen.getByText('ToDo 3');
+    expect(toDoItem3.getAttribute('href')).toBe('/detail/2');
     expect(screen.getByText('ToDo 1')).toBeInTheDocument();
     expect(screen.getByText('ToDo 2')).toBeInTheDocument();
     expect(screen.getByText('ToDo 3')).toBeInTheDocument();
@@ -91,9 +107,11 @@ describe('ToDoList Context', () => {
     };
 
     render(
-      <ToDoListProvider>
-        <ChildComponent />
-      </ToDoListProvider>,
+      <Router>
+        <ToDoListProvider>
+          <ChildComponent />
+        </ToDoListProvider>
+      </Router>,
     );
 
     const toDoItem = screen.getByText('ToDo 2');
@@ -101,5 +119,32 @@ describe('ToDoList Context', () => {
     fireEvent.click(toDoItem);
     expect(toDoItem).not.toBeInTheDocument();
     expect(JSON.parse(localStorage.getItem('ToDoList') as string)).not.toContain('ToDo 2');
+  });
+
+  it('moves to detail page', () => {
+    const TestComponent = (): JSX.Element => {
+      const { pathname } = useLocation();
+      return <div>{pathname}</div>;
+    };
+
+    localStorage.setItem('ToDoList', '["ToDo 1","ToDo 2","ToDo 3"]');
+
+    render(
+      <Router>
+        <TestComponent />
+        <ToDoListProvider>
+          <ToDoList />
+        </ToDoListProvider>
+      </Router>,
+    );
+
+    const url = screen.getByText('/');
+    expect(url).toBeInTheDocument();
+
+    const toDoItem1 = screen.getByText('ToDo 2');
+    expect(toDoItem1.getAttribute('href')).toBe('/detail/1');
+    fireEvent.click(toDoItem1);
+
+    expect(url.textContent).toBe('/detail/1');
   });
 });
